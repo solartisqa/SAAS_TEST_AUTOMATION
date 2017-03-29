679a1093d9c6b82dd8b6c3b49c4b3797823f5db7
Sasirekha-Solartis <sharath_n@solartis.net>
2017-03-29 18:22:24 +0530

    03/29/2017-condition checking
    Condition reading class fully checked.

--- KeywordDrivenFramework/src/main/java/SupportingClasses/ConditionsChecking.java
+++ KeywordDrivenFramework/src/main/java/SupportingClasses/ConditionsChecking.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class ConditionsChecking {
 	
-	public boolean condition_reading(String condition,databaseOperartions input,databaseOperartions output) throws SQLException
+	public boolean condition_reading(String condition,databaseOperartions input) throws SQLException
 	{
-		boolean condition_reading=true;
+		boolean condition_reading=false;
 		
 		
 		if(condition.equals(""))
@@ -30,6 +30,7 @@ public class ConditionsChecking {
 		
 		for(int i=0;i<length;i++)
 		{
+			condition_reading=false;
 			String[] cond_value = new String[10];
 			String operator = null;
 			if(splits[i].contains("="))
@@ -55,23 +56,30 @@ public class ConditionsChecking {
 				//System.out.println(individualValue[j]);
 				switch(operator)
 				{
-				case "=": if(!(input.read_data(cond).equals(individualValue[j])))
-							{
+				case "=": if((input.read_data(cond).equals(individualValue[j])))
+						   {
 					//System.out.println("condition satisfied");
-					 			condition_reading=false;
-					 			return condition_reading;
+					 			condition_reading=true;
+					 			//return condition_reading;
 							}
 							break;
-				case "<>": if(input.read_data(cond).equals(individualValue[j]))
+				case "<>": if((input.read_data(cond).equals(individualValue[j])))
 							{
 		//System.out.println("condition satisfied");
 		 						condition_reading=false;
 		 						return condition_reading;
 							}
+							else
+							{
+								condition_reading=true;
+							}
 							break;
 				}
 			}
-				
+			if(!condition_reading)
+			{
+				return condition_reading;
+			}
 		}	
 		
 	}
@@ -79,6 +87,27 @@ public class ConditionsChecking {
 	return condition_reading;
 		
 	}
-
+	
+public static void main(String args[]) throws ClassNotFoundException, SQLException
+{
+	propertiesHandle configFile = new propertiesHandle("A:/1 Projects/13 Starr Assist/PDF Change/Configuration/Config_C1128.properties");
+	databaseOperartions.conn_setup(configFile);
+	System.setProperty("jsse.enableSNIExtension", "false");
+	String condition="State_of_Residence<>Alabama|Alaska;Plan_Name<>Air Ticket Protecto;";
+	databaseOperartions objectInput = new databaseOperartions();
+	objectInput.get_dataobjects(configFile.getProperty("input_query"));
+	 do
+		{
+		 
+		  if(objectInput.read_data("flag_for_execution").equals("Y"))
+			{  
+			  ConditionsChecking obj=new ConditionsChecking();
+			  System.out.println(obj.condition_reading(condition, objectInput));
+			}
+		}while(objectInput.move_forward()); //end of do
+	   databaseOperartions.close_conn(); 
+	
+	
+}
 
 }
