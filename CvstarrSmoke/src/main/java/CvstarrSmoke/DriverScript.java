20678cee29c7d3ceb99a68a4194e13a2a3d5b0b6
Sharath-Solartis <vigneshKumar_p@adsolartis.local>
2017-04-17 13:29:44 +0530

    for test

--- CvstarrSmoke/src/main/java/CvstarrSmoke/DriverScript.java
+++ CvstarrSmoke/src/main/java/CvstarrSmoke/DriverScript.java
@@ -32,10 +32,10 @@ public static void main(String args[]) throws ClassNotFoundException, SQLExcepti
 	    File jarFile = new File(DriverScript.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 	    System.out.println(jarFile);
 	    File FolderFile = jarFile.getParentFile();
-	    System.out.println(FolderFile);
-		propertiesHandle configFile = new propertiesHandle(FolderFile + "/BAD_Config.properties");
-		configFile.setProperty("driver_path", FolderFile +"/Drivers/");
-		configFile.setProperty("Test_script_path",FolderFile +"/");
+	    System.out.println(FolderFile + "/BAD_Config.properties");
+		propertiesHandle configFile = new propertiesHandle(FolderFile + "\\BAD_Config.properties");
+		configFile.setProperty("driver_path", FolderFile +"\\Drivers\\");
+		configFile.setProperty("Test_script_path",FolderFile +"\\");
 		System.setProperty("jsse.enableSNIExtension", "false");	
 		DriverScript objDriver=new DriverScript(configFile);
 		objDriver.launchBrowser();
