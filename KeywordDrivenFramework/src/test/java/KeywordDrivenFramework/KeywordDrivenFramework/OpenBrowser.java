2bdd6a3c78dac1e4cc53976a69277bba18620bbd
Sharath-Solartis <sharath_n@solartis.net>
2018-01-17 11:15:54 +0530

    commit-01172018
    sample class added

---
+++ KeywordDrivenFramework/src/test/java/KeywordDrivenFramework/KeywordDrivenFramework/OpenBrowser.java
@@ -0,0 +1,15 @@
+package KeywordDrivenFramework.KeywordDrivenFramework;
+
+import org.testng.annotations.Test;
+
+import com.selenium.Test.BaseSuite;
+
+public class OpenBrowser extends BaseSuite
+{
+
+	@Test
+	public void Hello()
+	{
+		System.out.println("hello");
+	}
+}
